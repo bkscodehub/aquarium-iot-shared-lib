7e9bff7a95c071b913af6fc1504f592218c4eaba
karthik <karthikeyan.tandl@gmail.com>
2025-05-29 16:13:58 +0530

    util method to check time in range

--- include/time_utils.h
+++ include/time_utils.h
@@ -7,5 +7,6 @@ void setClock();
 String getTimestamp();  // Get datetime in UTC
 int getHourNow();
 int getMinuteNow();
+bool isTimeInRange(int onHour, int onMinute, int offHour, int offMinute);
 
 #endif
